5797edf6b8b032174b989ea822cf8e27f50f0be9
Aorathai27 <150641735+Aorathai27@users.noreply.github.com>
2023-12-18 12:10:21 +0700

    w5

---
+++ database/migrations/2023_12_18_105742_create_foods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('a')->nullable();
+            $table->float('b')->nullable();
+            $table->string('c')->nullable();
+            $table->date('d')->nullable();
+            $table->text('e')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('foods');
+    }
+}
