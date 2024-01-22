2b05597e942c4bfecd15d746be6e791efbf58856
Aorathai27 <150641735+Aorathai27@users.noreply.github.com>
2024-01-22 12:21:16 +0700

    9

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\MyProfileController;
 use App\Http\Controllers\QuizController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -135,7 +137,10 @@ Route::get("study-question", [QuizController::class, "question"])->name("study-q
 Route::post("study-match", [QuizController::class, "match"])->name("study-match");
 use App\Http\Controllers\Covid19Controller;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StaffController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VehicleController;
 use App\Models\Staff;
 
 Route::get('/covid19', [ Covid19Controller::class,"index" ]);
@@ -157,3 +162,12 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
+//Route::resource('post', 'PostController');
+Route::resource('post', PostController::class);
+//Route::resource('profile', 'ProfileController');
+//Route::resource('user', 'UserController');
+//Route::resource('vehicle', 'VehicleController');
+
+Route::resource('profile', ProfileController::class);
+Route::resource('user', UserController::class);
+Route::resource('vehicle', VehicleController::class);
